80f9e0232a44e32f050d77671a5b0f40c1ec3fcf
starlighta841-ship-it <starlighta841@gmail.com>
2026-03-10 02:47:30 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showsidebar(){
+            const navb = document.querySelector('.navb');
+            navb.style.display = 'flex';
+        }
+        function hidesidebar(){
+            const navb = document.querySelector('.navb');
+            navb.style.display = 'none';
+        }
+
+alert("this is a alert box");
+document.body.style.background = "red";
